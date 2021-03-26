c752aafb8220f9109dacca813a9c4f48698f870e
Jasdac <JasXSL@users.noreply.github.com>
2021-03-26 19:07:19 +0100

    added console cheat cheat

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,40 +1,44 @@
-const CACHE_NAME = 'fq_cache';
-const urlsToCache = [
-	'/media/ui/logo_64.png',
-	'/media/ui/logo_128.png',
-	'/media/ui/logo_256.png',
-	'/media/ui/logo_512.png',
-	'/media/ui/marble.jpg',
-];
+if( self.location.hostname.split('.')[0].toLowerCase() === 'fetishquest' ){
 
-self.addEventListener('install', function(event) {
-	console.log("ITWERKS!!");
+	const CACHE_NAME = 'fq_cache';
+	const urlsToCache = [
+		'/media/ui/logo_64.png',
+		'/media/ui/logo_128.png',
+		'/media/ui/logo_256.png',
+		'/media/ui/logo_512.png',
+		'/media/ui/marble.jpg',
+	];
 
-	// Perform install steps
-	event.waitUntil(
-		caches.open(CACHE_NAME)
-		.then(cache => {
-			console.log('Opened cache');
-			return cache.addAll(urlsToCache);
-		}).then(() => {
-			console.log("Caches installed");
-		}).catch(err => {
-			console.error("Unable to add cache", err);
-		})
-	);
-	
-});
+	self.addEventListener('install', function(event) {
+		console.log("ITWERKS!!");
 
-self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches.match(event.request)
-			.then(function(response) {
-				// Cache hit - return response
-				if (response) {
-					return response;
+		// Perform install steps
+		event.waitUntil(
+			caches.open(CACHE_NAME)
+			.then(cache => {
+				console.log('Opened cache');
+				return cache.addAll(urlsToCache);
+			}).then(() => {
+				console.log("Caches installed");
+			}).catch(err => {
+				console.error("Unable to add cache", err);
+			})
+		);
+		
+	});
+
+	self.addEventListener('fetch', function(event) {
+		event.respondWith(
+			caches.match(event.request)
+				.then(function(response) {
+					// Cache hit - return response
+					if (response) {
+						return response;
+					}
+					return fetch(event.request);
 				}
-				return fetch(event.request);
-			}
-		)
-	);
-});
+			)
+		);
+	});
+
+}
